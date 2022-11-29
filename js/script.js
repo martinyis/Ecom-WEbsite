906a678585e207894aa8374bc0b163de89c22a8d
martinyis <martinyis11@gmail.com>
2022-11-29 18:11:20 -0500

    Adding shopping page to website

--- js/script.js
+++ js/script.js
@@ -1,51 +1,59 @@
-console.log("helo");
+console.log('helo');
 
 //Default vatiables
-const menu = document.querySelector(".header__menu");
-const button = document.querySelector(".demo-cont");
-const tracker = document.querySelector(".header__basket-tracker");
-const footerForm = document.querySelector(".footer__form");
-const footerLink = document.querySelectorAll(".footer__link");
+const menu = document.querySelector('.header__menu');
+const button = document.querySelector('.demo-cont');
+const tracker = document.querySelector('.header__basket-tracker');
+const footerForm = document.querySelector('.footer__form');
+const footerLink = document.querySelectorAll('.footer__link');
 //choose all document
-const body = document.querySelector("body");
-const headerBasker = document.querySelector(".header__basket");
+const body = document.querySelector('body');
+const headerBasker = document.querySelector('.header__basket');
 
 //Produtcs variables===========================================================================
-const product = document.querySelectorAll(".product");
-const burgerIcon = document.querySelector(".demo-cont");
-const about = document.querySelector(".header-about");
-
+const product = document.querySelectorAll('.product');
+const burgerIcon = document.querySelector('.demo-cont');
+const about = document.querySelector('.header-about');
+const shopBtns = document.querySelectorAll('.shop-link');
 //Default functions============================================================================================
 footerLink.forEach((item) => {
-  item.addEventListener("click", (e) => {
+  item.addEventListener('click', (e) => {
     e.preventDefault();
   });
 });
-footerForm.addEventListener("click", function (e) {
+footerForm.addEventListener('click', function (e) {
   e.preventDefault();
 });
-tracker.innerHTML = JSON.parse(localStorage.getItem("tracker"))
-  ? JSON.parse(localStorage.getItem("tracker"))
+tracker.innerHTML = JSON.parse(localStorage.getItem('tracker'))
+  ? JSON.parse(localStorage.getItem('tracker'))
   : 0;
 //BurgerMenu functions===========================================================================================
-menu.classList.add("disactive");
-button.addEventListener("click", function () {
-  menu.classList.toggle("active");
-  body.classList.toggle("lock");
-  headerBasker.classList.toggle("minus-zindex");
+menu.classList.add('disactive');
+button.addEventListener('click', function () {
+  menu.classList.toggle('active');
+  body.classList.toggle('lock');
+  headerBasker.classList.toggle('minus-zindex');
+});
+
+//Adding a link to shop buttons=====================================================================================\
+const shopLink = function () {
+  window.location.href = '../shop.html';
+};
+shopBtns.forEach((item) => {
+  item.addEventListener('click', shopLink);
 });
 //Render page product=======================================================
 const renderProduct = function (e) {
-  window.location.href = "../product.html";
+  window.location.href = '../product.html';
   let name = e.target
-    .closest(".product")
-    .querySelector(".product-name").textContent;
+    .closest('.product')
+    .querySelector('.product-name').textContent;
   //get pure text from a name
   name = name.trim();
   let price = e.target
-    .closest(".product")
-    .querySelector(".product-price").textContent;
-  let imgPath = e.target.closest(".product").querySelector("img").src;
+    .closest('.product')
+    .querySelector('.product-price').textContent;
+  let imgPath = e.target.closest('.product').querySelector('img').src;
   console.log(name, price, imgPath);
   let info = {
     name: name,
@@ -53,10 +61,10 @@ const renderProduct = function (e) {
     imgPath: imgPath,
   };
   // add info to localstorage
-  localStorage.setItem("info", JSON.stringify(info));
+  localStorage.setItem('info', JSON.stringify(info));
 };
 product.forEach((item) => {
-  item.addEventListener("click", renderProduct);
+  item.addEventListener('click', renderProduct);
 });
 
 // localStorage.clear();
