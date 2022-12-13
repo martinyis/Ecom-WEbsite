0662e131054d1f6384c0f67cc5178a1fbcb48c5b
martinyis <martinyis11@gmail.com>
2022-12-13 18:05:27 -0500

    Adding a function which renders a products from give object

---
+++ js/resourses/clothes.js
@@ -0,0 +1,17 @@
+export const clothes = [
+  {
+    name: 'Felted Wool-blend Car Coat',
+    price: 129,
+    image: '../../img/products/felted-wool-blend-car-coat.jpg',
+  },
+  {
+    name: 'Regular Fit Velvet Shirt',
+    price: 49.99,
+    image: '../../img/products/regular-fit-velvet-shirt.jpg',
+  },
+  {
+    name: 'Regular Fit Flared Pants',
+    price: 49.99,
+    image: '../../img/products/regular-fir-flared-pants.jpg',
+  },
+];
